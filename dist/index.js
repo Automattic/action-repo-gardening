6d55f28cfa8527007d4571785552f2a00477abc4
jeherve <jeherve@users.noreply.github.com>
2025-01-22 10:15:54 +0000

    Repo gardening: don't send slack notifs for closed issues (#41165)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12905715134
    
    Upstream-Ref: Automattic/jetpack@dbbc08544ed124181e4b8a7eaf87dd76525ee258

--- dist/index.js
+++ dist/index.js
@@ -50137,7 +50137,7 @@ async function createOrUpdateComment( payload, octokit, issueReferences, issueCo
  */
 async function addHappinessLabel( payload, octokit ) {
 	const {
-		issue: { number },
+		issue: { number, state },
 		repository: {
 			name: repo,
 			owner: { login: ownerLogin },
@@ -50164,9 +50164,10 @@ async function addHappinessLabel( payload, octokit ) {
 
 	// Send Slack notification, if we have the necessary tokens.
 	// No Slack tokens, we won't be able to escalate. Bail.
+	// If the issue is already closed, do not send any Slack reminder.
 	const slackToken = getInput( 'slack_token' );
 	const channel = getInput( 'slack_quality_channel' );
-	if ( ! slackToken || ! channel ) {
+	if ( ! slackToken || ! channel || state === 'closed' ) {
 		return false;
 	}
 
