7ba4e9aa3e6f964b71f13f6edd20baa04d58253f
anomiex <anomiex@users.noreply.github.com>
2025-04-01 18:51:56 +0000

    Lock file maintenance (#42815)
    * Lock file maintenance
    * Remove override for `@automattic/calypso-config`
    * Downgrade `@automattic/data-stores` to 3.1.0
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14203395476
    
    Upstream-Ref: Automattic/jetpack@2f3e99eb26208d17e42555979122e660f370998a

--- dist/index.js
+++ dist/index.js
@@ -2070,9 +2070,9 @@ exports.getOctokitOptions = exports.GitHub = exports.defaults = exports.context 
 const Context = __importStar(__nccwpck_require__(7798));
 const Utils = __importStar(__nccwpck_require__(916));
 // octokit + plugins
-const core_1 = __nccwpck_require__(8426);
-const plugin_rest_endpoint_methods_1 = __nccwpck_require__(2418);
-const plugin_paginate_rest_1 = __nccwpck_require__(6840);
+const core_1 = __nccwpck_require__(9270);
+const plugin_rest_endpoint_methods_1 = __nccwpck_require__(6940);
+const plugin_paginate_rest_1 = __nccwpck_require__(3755);
 exports.context = new Context.Context();
 const baseUrl = Utils.getApiBaseUrl();
 exports.defaults = {
@@ -3533,7 +3533,7 @@ var createTokenAuth = function createTokenAuth2(token) {
 
 /***/ }),
 
-/***/ 8426:
+/***/ 9270:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 "use strict";
@@ -3557,11 +3557,11 @@ var __copyProps = (to, from, except, desc) => {
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // pkg/dist-src/index.js
-var dist_src_exports = {};
-__export(dist_src_exports, {
+var index_exports = {};
+__export(index_exports, {
   Octokit: () => Octokit
 });
-module.exports = __toCommonJS(dist_src_exports);
+module.exports = __toCommonJS(index_exports);
 var import_universal_user_agent = __nccwpck_require__(7125);
 var import_before_after_hook = __nccwpck_require__(6223);
 var import_request = __nccwpck_require__(2518);
@@ -3569,7 +3569,7 @@ var import_graphql = __nccwpck_require__(6859);
 var import_auth_token = __nccwpck_require__(8953);
 
 // pkg/dist-src/version.js
-var VERSION = "5.2.0";
+var VERSION = "5.2.1";
 
 // pkg/dist-src/index.js
 var noop = () => {
@@ -4243,7 +4243,7 @@ function withCustomRequest(customRequest) {
 
 /***/ }),
 
-/***/ 6840:
+/***/ 3755:
 /***/ ((module) => {
 
 "use strict";
@@ -4644,7 +4644,7 @@ paginateRest.VERSION = VERSION;
 
 /***/ }),
 
-/***/ 2418:
+/***/ 6940:
 /***/ ((module) => {
 
 "use strict";
@@ -59312,7 +59312,7 @@ exports.GlobStream = GlobStream;
 
 /***/ }),
 
-/***/ 7926:
+/***/ 2929:
 /***/ ((__unused_webpack_module, exports) => {
 
 "use strict";
@@ -59465,6 +59465,7 @@ class LRUCache {
     #max;
     #maxSize;
     #dispose;
+    #onInsert;
     #disposeAfter;
     #fetchMethod;
     #memoMethod;
@@ -59546,6 +59547,7 @@ class LRUCache {
     #hasDispose;
     #hasFetchMethod;
     #hasDisposeAfter;
+    #hasOnInsert;
     /**
      * Do not call this method unless you need to inspect the
      * inner workings of the cache.  If anything returned by this
@@ -59623,13 +59625,19 @@ class LRUCache {
         return this.#dispose;
     }
     /**
+     * {@link LRUCache.OptionsBase.onInsert} (read-only)
+     */
+    get onInsert() {
+        return this.#onInsert;
+    }
+    /**
      * {@link LRUCache.OptionsBase.disposeAfter} (read-only)
      */
     get disposeAfter() {
         return this.#disposeAfter;
     }
     constructor(options) {
-        const { max = 0, ttl, ttlResolution = 1, ttlAutopurge, updateAgeOnGet, updateAgeOnHas, allowStale, dispose, disposeAfter, noDisposeOnSet, noUpdateTTL, maxSize = 0, maxEntrySize = 0, sizeCalculation, fetchMethod, memoMethod, noDeleteOnFetchRejection, noDeleteOnStaleGet, allowStaleOnFetchRejection, allowStaleOnFetchAbort, ignoreFetchAbort, } = options;
+        const { max = 0, ttl, ttlResolution = 1, ttlAutopurge, updateAgeOnGet, updateAgeOnHas, allowStale, dispose, onInsert, disposeAfter, noDisposeOnSet, noUpdateTTL, maxSize = 0, maxEntrySize = 0, sizeCalculation, fetchMethod, memoMethod, noDeleteOnFetchRejection, noDeleteOnStaleGet, allowStaleOnFetchRejection, allowStaleOnFetchAbort, ignoreFetchAbort, } = options;
         if (max !== 0 && !isPosInt(max)) {
             throw new TypeError('max option must be a nonnegative integer');
         }
@@ -59673,6 +59681,9 @@ class LRUCache {
         if (typeof dispose === 'function') {
             this.#dispose = dispose;
         }
+        if (typeof onInsert === 'function') {
+            this.#onInsert = onInsert;
+        }
         if (typeof disposeAfter === 'function') {
             this.#disposeAfter = disposeAfter;
             this.#disposed = [];
@@ -59682,6 +59693,7 @@ class LRUCache {
             this.#disposed = undefined;
         }
         this.#hasDispose = !!this.#dispose;
+        this.#hasOnInsert = !!this.#onInsert;
         this.#hasDisposeAfter = !!this.#disposeAfter;
         this.noDisposeOnSet = !!noDisposeOnSet;
         this.noUpdateTTL = !!noUpdateTTL;
@@ -60249,6 +60261,9 @@ class LRUCache {
             if (status)
                 status.set = 'add';
             noUpdateTTL = false;
+            if (this.#hasOnInsert) {
+                this.#onInsert?.(v, k, 'add');
+            }
         }
         else {
             // update
@@ -60289,6 +60304,9 @@ class LRUCache {
             }
             else if (status) {
                 status.set = 'update';
+            }
+            if (this.#hasOnInsert) {
+                this.onInsert?.(v, k, v === oldVal ? 'update' : 'replace');
             }
         }
         if (ttl !== 0 && !this.#ttls) {
@@ -68623,7 +68641,7 @@ const resource_1 = __nccwpck_require__(5530);
 const core_1 = __nccwpck_require__(7295);
 const core_2 = __nccwpck_require__(7295);
 const Util_1 = __nccwpck_require__(5694);
-const files_1 = __nccwpck_require__(3755);
+const files_1 = __nccwpck_require__(8383);
 Object.defineProperty(exports, "VectorStoreFilesPage", ({ enumerable: true, get: function () { return files_1.VectorStoreFilesPage; } }));
 class FileBatches extends resource_1.APIResource {
     /**
@@ -68747,7 +68765,7 @@ exports.FileBatches = FileBatches;
 
 /***/ }),
 
-/***/ 3755:
+/***/ 8383:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -68780,7 +68798,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.VectorStoreFilesPage = exports.Files = void 0;
 const resource_1 = __nccwpck_require__(5530);
 const core_1 = __nccwpck_require__(7295);
-const FilesAPI = __importStar(__nccwpck_require__(3755));
+const FilesAPI = __importStar(__nccwpck_require__(8383));
 const pagination_1 = __nccwpck_require__(5461);
 class Files extends resource_1.APIResource {
     /**
@@ -68937,7 +68955,7 @@ const resource_1 = __nccwpck_require__(5530);
 const core_1 = __nccwpck_require__(7295);
 const VectorStoresAPI = __importStar(__nccwpck_require__(6682));
 const FileBatchesAPI = __importStar(__nccwpck_require__(9471));
-const FilesAPI = __importStar(__nccwpck_require__(3755));
+const FilesAPI = __importStar(__nccwpck_require__(8383));
 const pagination_1 = __nccwpck_require__(5461);
 class VectorStores extends resource_1.APIResource {
     constructor() {
@@ -70500,7 +70518,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.PathScurry = exports.Path = exports.PathScurryDarwin = exports.PathScurryPosix = exports.PathScurryWin32 = exports.PathScurryBase = exports.PathPosix = exports.PathWin32 = exports.PathBase = exports.ChildrenCache = exports.ResolveCache = void 0;
-const lru_cache_1 = __nccwpck_require__(7926);
+const lru_cache_1 = __nccwpck_require__(2929);
 const node_path_1 = __nccwpck_require__(9411);
 const node_url_1 = __nccwpck_require__(1041);
 const fs_1 = __nccwpck_require__(7147);
